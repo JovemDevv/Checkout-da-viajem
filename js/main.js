d159c0e8d4f16e579ace6a72aa1e205bf59eb9f7
JovemDevv <YOUR_EMAIL>
2023-07-17 14:00:48 -0300

    Introduzindo o js e  a mensagem de finalização

---
+++ js/main.js
@@ -0,0 +1,17 @@
+document.getElementById("checkout-form").addEventListener("submit", function(event) {
+    event.preventDefault(); // Impede o envio do formulário
+  
+    // Exibir a mensagem de sucesso em um elemento separado
+    var successMessage = document.createElement("div");
+    successMessage.textContent = "Checkout finalizado com sucesso!";
+    successMessage.classList.add("success-message"); // Adiciona a classe "success-message" ao elemento
+  
+    // Adicionar a mensagem ao body do documento
+    document.body.appendChild(successMessage);
+  
+    // Recarregar a página após 2 segundos
+    setTimeout(function() {
+      location.reload();
+    }, 2000);
+  });
+  
